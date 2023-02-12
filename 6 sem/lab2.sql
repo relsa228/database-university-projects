83a073a018bcf99d547318243f305d97b21e425c
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2023-02-12 05:12:34 +0300

    шестой триггер

--- 6 sem/lab2.sql
+++ 6 sem/lab2.sql
@@ -43,3 +43,16 @@ END;
 --5. Восстановления информации
 
 --6. Триггер на синхронизацию
+CREATE OR REPLACE TRIGGER LAB2.sync 
+AFTER INSERT OR DELETE OR UPDATE ON LAB2."students"
+FOR EACH ROW
+BEGIN
+    IF inserting THEN
+        UPDATE LAB2."groups" SET c_val=c_val+1 WHERE "id" = :NEW.group_id;
+    ELSIF updating THEN
+        UPDATE LAB2."groups" SET c_val=c_val+1 WHERE "id" = :NEW.group_id;
+        UPDATE LAB2."groups" SET c_val=c_val-1 WHERE "id" = :OLD.group_id;
+    ELSIF deleting THEN
+        UPDATE LAB2."groups" SET c_val=c_val-1 WHERE "id" = :OLD.group_id;
+    END IF;
+END;
